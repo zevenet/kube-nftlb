98d56954498e1182c32aa919d6817ceb87958c55
AquoDev <14019792+AquoDev@users.noreply.github.com>
2018-10-22 17:16:59 +0200

    Updated build.sh and .yaml file

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/bin/sh
+
+# It lets you interact with nftlb.
+# The key must be propagated to the client and the daemon.
+key="12345"
 
 # Before running this script, you MUST BE ROOT and you need to have the following tools installed:
 #   - Docker
@@ -10,13 +14,22 @@
 # STEP 1: 
 #   Compile cmd/app/main.go.
 #   The binary will be called "app".
-GOOS=linux go build -o ./app ./cmd/app
+GOOS=linux go build -o ./docker/client/app ./cmd/app
 
 # YOU MUST DO THIS STEP IF YOU DIDN'T STARTED MINIKUBE WITH 'minikube start --vm-driver=none':
 #   Uncomment the line below if you apply.
 #eval $(minikube docker-env)
 
 # STEP 2:
-#   A container will be created using the attached Dockerfile.
+#   The client container will be created using its Dockerfile.
 #   It will be made for Docker, not for Minikube (this will come later).
-docker build -t nftlb .
+docker build -t client docker/client --build-arg KEY=$key
+
+# STEP 3:
+#   The daemon container will be created using its Dockerfile.
+#   It will be made for Docker, not for Minikube (this will come later).
+docker build -t daemon docker/daemon --build-arg KEY=$key
+
+# STEP 4:
+#   Clean residual files.
+rm -f docker/client/app
