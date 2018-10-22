0277a60fb98a772990a98f11de7702befd5a9baf
AquoDev <14019792+AquoDev@users.noreply.github.com>
2018-10-22 17:35:41 +0200

    Updated build.sh

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ key="12345"
 # STEP 1: 
 #   Compile cmd/app/main.go.
 #   The binary will be called "app".
-GOOS=linux go build -o ./docker/client/app ./cmd/app
+GOOS=linux go build -o ./internal/docker/client/app ./cmd/app
 
 # YOU MUST DO THIS STEP IF YOU DIDN'T STARTED MINIKUBE WITH 'minikube start --vm-driver=none':
 #   Uncomment the line below if you apply.
@@ -23,12 +23,12 @@ GOOS=linux go build -o ./docker/client/app ./cmd/app
 # STEP 2:
 #   The client container will be created using its Dockerfile.
 #   It will be made for Docker, not for Minikube (this will come later).
-docker build -t client docker/client --build-arg KEY=$key
+docker build -t client internal/docker/client --build-arg KEY=$key
 
 # STEP 3:
 #   The daemon container will be created using its Dockerfile.
 #   It will be made for Docker, not for Minikube (this will come later).
-docker build -t daemon docker/daemon --build-arg KEY=$key
+docker build -t daemon internal/docker/daemon --build-arg KEY=$key
 
 # STEP 4:
 #   Clean residual files.
