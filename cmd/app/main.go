6b5efa9bb66f43a40a3445eb26fd2e3082bc2a7d
AquoDev <14019792+AquoDev@users.noreply.github.com>
2018-10-27 11:01:44 +0200

    Reflect changes in main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,22 +3,25 @@ package main
 import (
 	"fmt"
 
+	watchers "github.com/zevenet/kube-nftlb/pkg/watchers"
 	auth "github.com/zevenet/kube-nftlb/pkg/auth"
-	svc "github.com/zevenet/kube-nftlb/pkg/svc"
 	wait "k8s.io/apimachinery/pkg/util/wait"
 )
 
 func main() {
 	// Authentication: get access to the API
 	clientset := auth.GetClienset()
-	// Make list of resources (every Service) to be observed
-	listWatch := svc.GetServiceListWatch(clientset)
+	// Make lists of resources to be observed
+	listWatchSvc := watchers.GetServiceListWatch(clientset)
+	listWatchEndpoint := watchers.GetEndpointListWatch(clientset)
 	// Make log channel before writing messages
 	logChannel := make(chan string)
-	// Notify every change into logChannel based on listWatch
-	serviceController := svc.GetServiceController(listWatch, logChannel)
-	// Event loop start, run it as background process
+	// Notify every change into logChannel based on every list watch
+	serviceController := watchers.GetServiceController(listWatchSvc, logChannel)
+	endpointController := watchers.GetEndpointController(listWatchEndpoint, logChannel)
+	// Event loop start, run them as background processes
 	go serviceController.Run(wait.NeverStop)
+	go endpointController.Run(wait.NeverStop)
 	// Print every message received from the channel
 	for message := range logChannel {
 		fmt.Println(message)
