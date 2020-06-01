b16f89ad86101d14975c2901f217a05c6c01a203
David-concejero <44636964+David-concejero@users.noreply.github.com>
2020-06-01 11:50:52 +0200

    Update encoding.go

--- pkg/json/encoding.go
+++ pkg/json/encoding.go
@@ -79,6 +79,96 @@ func GetJSONnftlbFromEndpoints(endpoints *v1.Endpoints) types.JSONnftlb {
 	CreateFarmID(farmName)
 	// Fills backends
 	var backends types.Backends
+
+	// obtener puerto
+		// puerto := endpoints.Subsets.Ports
+
+	for _, address := range addrSlice {
+		// Gets backend ID
+		backendID := GetBackendID(farmName)
+
+		// crear backend por cada puerto
+			for _, endpoint2 := range endpoints.Subsets {
+                		for _, port := range endpoint2.Ports {
+
+	                		var backend = types.Backend{
+                        			Name:   fmt.Sprintf("%s%d", farmName, backendID),
+                        			IPAddr: address,
+                        			State:  "up",
+						Port: fmt.Sprint(port.Port),
+                		}
+		                // Appends backend
+		                backends = append(backends, backend)
+                	}
+			// Increases backend ID
+                        IncreaseBackendID(farmName)
+        }
+	}
+	// Fills the farm
+	var farm = types.Farms{
+		types.Farm{
+			Name:     farmName,
+			Backends: backends,
+		},
+	}
+	// Returns the filled struct
+	return types.JSONnftlb{
+		Farms: farm,
+	}
+}
+
+// Contains returns true when "str" string is in "sl" slice.
+func Contains(sl []string, str string) bool {
+	for _, value := range sl {
+		if value == str {
+			return true
+		}
+	}
+	return false
+}
+	ports := strings.Join(portsSlice, ", ")
+	// Gets the protocol
+	protocol := ""
+	if protocolsSlice["TCP"] > 0 && protocolsSlice["UDP"] > 0 {
+		protocol = "all"
+	} else if protocolsSlice["TCP"] > 0 {
+		protocol = "tcp"
+	} else if protocolsSlice["UDP"] > 0 {
+		protocol = "udp"
+	}
+	// Fills the farm
+	var farm = types.Farms{
+		types.Farm{
+			Name:         farmName,
+			Family:       "ipv4",
+			VirtualAddr:  service.Spec.ClusterIP,
+			VirtualPorts: ports,
+			Mode:         "snat",
+			Protocol:     protocol,
+			State:        "up",
+			Backends:     types.Backends{},
+		},
+	}
+	// Returns the filled struct
+	return types.JSONnftlb{
+		Farms: farm,
+	}
+}
+
+// GetJSONnftlbFromEndpoints returns a JSONnftlb struct filled with any Endpoints data.
+func GetJSONnftlbFromEndpoints(endpoints *v1.Endpoints) types.JSONnftlb {
+	farmName := endpoints.ObjectMeta.Name
+	// Extracts individual addresses
+	var addrSlice []string
+	for _, endpoint := range endpoints.Subsets {
+		for _, address := range endpoint.Addresses {
+			addrSlice = append(addrSlice, address.IP)
+		}
+	}
+	// Initializes farm/backends ID
+	CreateFarmID(farmName)
+	// Fills backends
+	var backends types.Backends
 	for _, address := range addrSlice {
 		// Gets backend ID
 		backendID := GetBackendID(farmName)
