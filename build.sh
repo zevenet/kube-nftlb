b85118b41f0efdd03668dad736c855c12dcad0c8
Alvaro Cano <alvaro.cano@zevenet.com>
2020-06-03 12:53:26 +0200

    [Improvement] Use a unique container for nftlb daemon and GO client [Improvement] Allow using nftlb devel packages in build.sh
    Signed-off-by: Alvaro Cano <alvaro.cano@zevenet.com>
    
    	modificado:     .gitignore
    	modificado:     build.sh
    	modificado:     go.mod
    	modificado:     go.sum
    	borrado:        internal/docker/client/Dockerfile
    	renombrado:     internal/docker/daemon/Dockerfile -> internal/docker/kube-nftlb/Dockerfile
    	nuevo archivo:  internal/docker/kube-nftlb/start.sh
    	modificado:     yaml/create_nftlb_as_daemonset.yaml

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+DOCKER_PATH="./internal/docker/kube-nftlb"
+
+# Optionally, use a nftlb devel package
+if [ ! -z $1 ]; then
+	cp $1 $DOCKER_PATH/nftlb.deb
+else
+	# use empty file to avoid docker COPY directive failure
+	touch $DOCKER_PATH/nftlb.deb
+fi
+
+
 # It lets you interact with nftlb.
 # The key must be propagated to the client and the daemon.
 key="12345"
@@ -14,7 +25,7 @@ key="12345"
 # STEP 1:
 #   Compile cmd/app/main.go.
 #   The binary will be called "app".
-GOOS=linux go build -o ./internal/docker/client/app ./cmd/app
+GOOS=linux go build -o $DOCKER_PATH/app ./cmd/app
 
 # YOU MUST DO THIS STEP IF YOU DIDN'T STARTED MINIKUBE WITH 'minikube start --vm-driver=none':
 #   Uncomment the line below if you apply.
@@ -23,18 +34,14 @@ GOOS=linux go build -o ./internal/docker/client/app ./cmd/app
 # STEP 2:
 #   The client container will be created using its Dockerfile.
 #   It will be made for Docker, not for Minikube (this will come later).
-docker build -t client internal/docker/client --build-arg KEY=$key
+docker build -t kube-nftlb $DOCKER_PATH --build-arg KEY=$key
 
 # STEP 3:
-#   The daemon container will be created using its Dockerfile.
-#   It will be made for Docker, not for Minikube (this will come later).
-docker build -t daemon internal/docker/daemon --build-arg KEY=$key
+#   Clean residual files.
+rm -f $DOCKER_PATH/app
+rm $DOCKER_PATH/nftlb.deb
 
 # STEP 4:
-#   Clean residual files.
-rm -f internal/docker/client/app
-
-# STEP 5:
 #   Every nftables rule should be flushed before creating kube-nftlb.
 #   Do it at your own risk (you will probably have trouble with Docker).
 #nft flush ruleset
--- go.mod
+++ go.mod
@@ -3,11 +3,10 @@ module github.com/zevenet/kube-nftlb
 go 1.14
 
 require (
-        github.com/zevenet/kube-nftlb v0.0.0-20200512100317-ddc225153095
-        golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
-        golang.org/x/time v0.0.0-20200416051211-89c76fbcd5d1 // indirect
-        k8s.io/api v0.18.2
-        k8s.io/apimachinery v0.18.2
-        k8s.io/client-go v0.17.0
-        k8s.io/utils v0.0.0-20200414100711-2df71ebbae66 // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	golang.org/x/time v0.0.0-20200416051211-89c76fbcd5d1 // indirect
+	k8s.io/api v0.18.2
+	k8s.io/apimachinery v0.18.2
+	k8s.io/client-go v0.17.0
+	k8s.io/utils v0.0.0-20200414100711-2df71ebbae66 // indirect
 )
