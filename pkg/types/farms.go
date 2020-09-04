5e097ced7f5766a5da541f705e6915add1f44eb4
AquoDev <14019792+AquoDev@users.noreply.github.com>
2020-09-04 14:27:31 +0200

    [Bug fix] Declare JSON attributes for structs

--- pkg/types/farms.go
+++ pkg/types/farms.go
@@ -2,40 +2,37 @@ package types
 
 // Backend defines any backend with its properties.
 type Backend struct {
-	Name            string `json:"name"`
-	IPAddr          string `json:"ip-addr"`
-	Weight          string `json:"weight,omitempty"`
-	Priority        string `json:"priority,omitempty"`
-	Mark            string `json:"mark,omitempty"`
-	State           string `json:"state,omitempty"`
-	Port            string `json:"port,omitempty"`
-	BackendMaxConns string `json:"est-connlimit,omitempty"`
+	Name         string `json:"name"`
+	IPAddr       string `json:"ip-addr"`
+	Weight       string `json:"weight,omitempty"`
+	Priority     string `json:"priority,omitempty"`
+	Mark         string `json:"mark,omitempty"`
+	State        string `json:"state,omitempty"`
+	Port         string `json:"port,omitempty"`
+	EstConnlimit string `json:"est-connlimit,omitempty"`
 }
-
-// Backends defines a group of backends in any farm.
-type Backends []Backend
 
 // Farm defines any farm with its properties.
 type Farm struct {
-	Name           string    `json:"name"`
-	Family         string    `json:"family,omitempty"`
-	VirtualAddr    string    `json:"virtual-addr,omitempty"`
-	VirtualPorts   string    `json:"virtual-ports,omitempty"`
-	Mode           string    `json:"mode,omitempty"`
-	Protocol       string    `json:"protocol,omitempty"`
-	Scheduler      string    `json:"scheduler,omitempty"`
-	SchedulerParam string    `json:"sched-param,omitempty"`
-	Helper         string    `json:"helper,omitempty"`
-	Log            string    `json:"log,omitempty"`
-	LogPrefix      string    `json:"log-prefix,omitempty"`
-	Mark           string    `json:"mark,omitempty"`
-	Priority       string    `json:"priority,omitempty"`
-	State          string    `json:"state,omitempty"`
-	Intraconnect   string    `json:"intra-connect,omitempty"`
-	Persistence    string    `json:"persistence,omitempty"`
-	PersistTTL     string    `json:"persist-ttl,omitempty"`
-	Iface          string    `json:"iface,omitempty"`
-	Backends       []Backend `json:"backends"`
+	Name         string    `json:"name"`
+	Family       string    `json:"family,omitempty"`
+	VirtualAddr  string    `json:"virtual-addr,omitempty"`
+	VirtualPorts string    `json:"virtual-ports,omitempty"`
+	Mode         string    `json:"mode,omitempty"`
+	Protocol     string    `json:"protocol,omitempty"`
+	Scheduler    string    `json:"scheduler,omitempty"`
+	SchedParam   string    `json:"sched-param,omitempty"`
+	Helper       string    `json:"helper,omitempty"`
+	Log          string    `json:"log,omitempty"`
+	LogPrefix    string    `json:"log-prefix,omitempty"`
+	Mark         string    `json:"mark,omitempty"`
+	Priority     string    `json:"priority,omitempty"`
+	State        string    `json:"state,omitempty"`
+	IntraConnect string    `json:"intra-connect,omitempty"`
+	Persistence  string    `json:"persistence,omitempty"`
+	PersistTTL   string    `json:"persist-ttl,omitempty"`
+	Iface        string    `json:"iface,omitempty"`
+	Backends     []Backend `json:"backends"`
 }
 
 // Farms defines a struct made for nftlb requests.
--- pkg/types/policies.go
+++ pkg/types/policies.go
@@ -2,20 +2,19 @@ package types
 
 // Element defines what's inside a policy "elements" attribute.
 type Element struct {
-	Data string
+	Data string `json:"data"`
 }
 
 // Policy defines what's a policy nftlb object with its attributes.
 type Policy struct {
-	Name      string
-	Type      string
-	Traffic   string
-	Family    string
-	LogPrefix string
-	Elements  []Element
+	Name      string    `json:"name"`
+	Type      string    `json:"type"`
+	Family    string    `json:"family"`
+	LogPrefix string    `json:"log-prefix,omitempty"`
+	Elements  []Element `json:"elements"`
 }
 
 // Policies defines the "policies" nftlb object.
 type Policies struct {
-	Policies []Policy
+	Policies []Policy `json:"policies"`
 }
