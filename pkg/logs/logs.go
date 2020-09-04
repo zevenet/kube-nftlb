4b5b8fd86c8090700246b2dcaa4a9a76454672a9
AquoDev <14019792+AquoDev@users.noreply.github.com>
2020-09-04 08:10:40 +0200

    [Improvement] Refactor logs package

--- pkg/logs/logs.go
+++ pkg/logs/logs.go
@@ -6,11 +6,13 @@ import (
 	"github.com/zevenet/kube-nftlb/pkg/config"
 )
 
+var logLevel = config.ClientLevelLogs
+
 func WriteLogFuncGeneral(levelLog int, action string, resourceName string, obj interface{}) {
 	// Print the logs of the add or remove actions of the funcs. Every time a service is created or deleted these logs will appear
 	// In essence it prints the action (either delete or add) of the resource and prints the content of the object.
 	// See watchers.go
-	if checkLevelLogs(levelLog) == true {
+	if checkLevelLogs(levelLog) {
 		message := fmt.Sprintf("\n"+action+"%s:\n%s", resourceName, obj)
 		fmt.Println(message)
 	}
@@ -20,7 +22,7 @@ func WriteLogFuncUpdate(levelLog int, resourceName string, oldObj interface{}, n
 	// Print the logs of the update actions. In this case it prints both the original data and after doing the update
 	// In essence it prints the action of the resource and prints the content of the object before and after being updated
 	// See watchers.go
-	if checkLevelLogs(levelLog) == true {
+	if checkLevelLogs(levelLog) {
 		message := fmt.Sprintf("\nUpdatedd %s:\n* BEFORE: %s\n* NOW: %s", resourceName, oldObj, newObj)
 		fmt.Println(message)
 	}
@@ -28,17 +30,11 @@ func WriteLogFuncUpdate(levelLog int, resourceName string, oldObj interface{}, n
 
 func WriteLog(levelLog int, message string) {
 	// Within the add function, all the information we show about the process
-	if checkLevelLogs(levelLog) == true {
+	if checkLevelLogs(levelLog) {
 		fmt.Println(message)
 	}
 }
 
 func checkLevelLogs(levelPrint int) bool {
-	// Reads the level of logs established within the parameterizable values
-	// If the level of logs is less than or equal to the established one, it returns true and gives the green light to print logs.
-	logLevel := config.ClientLevelLogs
-	if levelPrint <= logLevel {
-		return true
-	}
-	return false
+	return levelPrint <= logLevel
 }
