f60faf0a85982c34b9d7bb844d0263037a891d7c
AquoDev <14019792+AquoDev@users.noreply.github.com>
2020-09-04 14:27:59 +0200

    [Bug fix] Pass an empty context instead of nil

--- pkg/parser/endpoints.go
+++ pkg/parser/endpoints.go
@@ -1,136 +1,138 @@
 package parser
 
 import (
+	"context"
 	"fmt"
 
-	"github.com/zevenet/kube-nftlb/pkg/auth"
 	"github.com/zevenet/kube-nftlb/pkg/dsr"
 	"github.com/zevenet/kube-nftlb/pkg/types"
+	"k8s.io/apimachinery/pkg/labels"
 
 	corev1 "k8s.io/api/core/v1"
-)
-
-var (
-	clienset = auth.GetClientset()
-
-	// Active endpoints per service
-	portsMap = map[string][]string{}
-
-	// Active nodeports per service
-	nodePortMap = map[string][]string{}
-
-	// Active externalIPs per service
-	externalIPsMap = map[string][]string{}
-
-	// Active number of maxconns per service in the backend
-	maxConnsMap = map[string]string{}
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // EndpointsAsFarms
-func EndpointsAsFarms(endpoints *corev1.Endpoints) types.Farms {
-	endpointName := endpoints.ObjectMeta.Name
+func EndpointsAsFarms(endpoints *corev1.Endpoints) *types.Farms {
+	endpointName := endpoints.Name
+	serviceList := getServiceListFromEndpoints(endpoints.Labels)
 	state := "up"
 	maxconns := "0"
-	var farmsSlice []types.Farm
-	// Go through each of the services we have created, specifically for each ip.
-	// Then create and assign a backend for each port of our service.
-	for _, endpoint := range endpoints.Subsets {
-		backendID := 0
-		for _, address := range endpoint.Addresses {
-			// Get the ip and the name of the backends. If the name field is empty, it is assigned the same as the service.
-			ipBackend := address.IP
-			backendName := endpoints.ObjectMeta.Name
-			if address.TargetRef != nil {
-				backendName = address.TargetRef.Name
-			}
+	farms := make([]types.Farm, 0)
 
-			// We proceed to create each of the backends
-			for _, port := range endpoint.Ports {
-				// If the port name field is empty, it is assigned one by default
-				if port.Name == "" {
-					port.Name = "default"
+	for _, service := range serviceList.Items {
+		serviceName := service.Name
+		for _, endpoint := range endpoints.Subsets {
+			for _, address := range endpoint.Addresses {
+				// Get the ip and the name of the backends. If the name field is empty, it is assigned the same as the service.
+				ipBackend := address.IP
+				backendName := endpoints.ObjectMeta.Name
+				if address.TargetRef != nil {
+					backendName = address.TargetRef.Name
 				}
 
-				// Attach the backends to the service
-				farmName := assignFarmNameService(endpointName, port.Name)
+				// We proceed to create each of the backends
+				for _, port := range endpoint.Ports {
+					// If the port name field is empty, it is assigned one by default
+					if port.Name == "" {
+						port.Name = "default"
+					}
 
-				// Get backend ports
-				portBackend := fmt.Sprint(port.Port)
-				if dsr.ExistsServiceDSR(farmName) {
-					dsr.AddInterfaceDsr(farmName, backendName, dsr.GetVirtualAddr(farmName))
-					portBackend = dsr.GetVirtualPorts(farmName)
-				}
+					// Attach the backends to the service
+					farmName := assignFarmNameService(endpointName, port.Name)
 
-				//
-				if maxConnsMap[farmName] != "0" {
-					maxconns = maxConnsMap[farmName]
-				}
+					// Get backend ports
+					portBackend := fmt.Sprint(port.Port)
+					if dsr.ExistsServiceDSR(farmName) {
+						dsr.AddInterfaceDsr(farmName, backendName, dsr.GetVirtualAddr(farmName))
+						portBackend = dsr.GetVirtualPorts(farmName)
+					}
 
-				backends := make([]types.Backend, 0)
-				backend := types.Backend{
-					Name:            backendName,
-					IPAddr:          ipBackend,
-					State:           state,
-					Port:            portBackend,
-					BackendMaxConns: maxconns,
-				}
-				backends = append(backends, backend)
-				var farm = types.Farm{
-					Name:     fmt.Sprintf("%s", farmName),
-					Backends: backends,
-				}
-				farmsSlice = append(farmsSlice, farm)
-				// Check if the current service is of type nodePort thanks to the global variable that we have created previously
-				// If this is the case, the nodePort service is assigned the same backends as the original service
-				nodePortFarmName := assignFarmNameNodePort(farmName, "nodePort")
-				if /*Contains(nodePortArray, nodePortFarmName)*/ true {
+					//
+					if maxConnsMap[serviceName][farmName] != "0" {
+						maxconns = maxConnsMap[serviceName][farmName]
+					}
+
+					backends := make([]types.Backend, 0)
+					backend := types.Backend{
+						Name:         backendName,
+						IPAddr:       ipBackend,
+						State:        state,
+						Port:         portBackend,
+						EstConnlimit: maxconns,
+					}
+					backends = append(backends, backend)
 					var farm = types.Farm{
-						Name:     fmt.Sprintf("%s", nodePortFarmName),
+						Name:     farmName,
 						Backends: backends,
 					}
-					farmsSlice = append(farmsSlice, farm)
-				}
+					farms = append(farms, farm)
 
-				// Check if the current service has externalIPs thanks to the global variable that we have created previously
-				// If this is the case, the externalIPs service is assigned the same backends as the original service
-				if _, ok := externalIPsMap[farmName]; ok {
-					for _, farmExternalIPs := range externalIPsMap[farmName] {
+					// Check if the current service is of type nodePort thanks to the global variable that we have created previously
+					// If this is the case, the nodePort service is assigned the same backends as the original service
+					nodePortFarmName := assignFarmNameNodePort(farmName, "nodePort")
+					if /*Contains(nodePortArray, nodePortFarmName)*/ true {
 						var farm = types.Farm{
-							Name:     fmt.Sprintf("%s", farmExternalIPs),
+							Name:     nodePortFarmName,
 							Backends: backends,
 						}
-						farmsSlice = append(farmsSlice, farm)
+						farms = append(farms, farm)
+					}
+
+					// Check if the current service has externalIPs thanks to the global variable that we have created previously
+					// If this is the case, the externalIPs service is assigned the same backends as the original service
+					if _, ok := externalIPsMap[serviceName]; ok {
+						for _, farmNameExternalIP := range externalIPsMap[farmName] {
+							var farm = types.Farm{
+								Name:     farmNameExternalIP,
+								Backends: backends,
+							}
+							farms = append(farms, farm)
+						}
 					}
 				}
-
-				backendID++
 			}
 		}
 	}
+
 	// Returns the filled struct
-	return types.Farms{
-		Farms: farmsSlice,
+	return &types.Farms{
+		Farms: farms,
 	}
 }
 
 // DeleteEndpointsBackends returns every backend name through a channel from a farm given a Endpoints object.
 func DeleteEndpointsBackends(endpoints *corev1.Endpoints, backendPathsChan chan<- string) {
-	farmName := endpoints.ObjectMeta.Name
+	serviceList := getServiceListFromEndpoints(endpoints.Labels)
 
-	for _, backendName := range portsMap[farmName] {
-		backendPathsChan <- fmt.Sprintf("farms/%s/backends/%s", farmName, backendName)
-	}
-	go delete(portsMap, farmName)
+	for _, service := range serviceList.Items {
+		farmName := service.Name
 
-	for _, backendName := range nodePortMap[farmName] {
-		backendPathsChan <- fmt.Sprintf("farms/%s/backends/%s", farmName, backendName)
-	}
-	go delete(nodePortMap, farmName)
+		for _, backendName := range portsMap[farmName] {
+			backendPathsChan <- fmt.Sprintf("farms/%s/backends/%s", farmName, backendName)
+		}
+		go delete(portsMap, farmName)
 
-	for _, backendName := range externalIPsMap[farmName] {
-		backendPathsChan <- fmt.Sprintf("farms/%s/backends/%s", farmName, backendName)
+		for _, backendName := range nodePortMap[farmName] {
+			backendPathsChan <- fmt.Sprintf("farms/%s/backends/%s", farmName, backendName)
+		}
+		go delete(nodePortMap, farmName)
+
+		for _, backendName := range externalIPsMap[farmName] {
+			backendPathsChan <- fmt.Sprintf("farms/%s/backends/%s", farmName, backendName)
+		}
+		go delete(externalIPsMap, farmName)
 	}
-	go delete(externalIPsMap, farmName)
 
 	close(backendPathsChan)
+}
+
+func getServiceListFromEndpoints(endpointsLabels map[string]string) *corev1.ServiceList {
+	opts := metav1.ListOptions{
+		LabelSelector: labels.Set(endpointsLabels).String(),
+	}
+
+	list, _ := clientset.CoreV1().Services(corev1.NamespaceAll).List(context.TODO(), opts)
+
+	return list
 }
