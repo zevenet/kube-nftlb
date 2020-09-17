3d30b02ecb38c3b82c4b313f90cae6fd9e14f36c
AquoDev <14019792+AquoDev@users.noreply.github.com>
2020-09-17 10:15:25 +0200

    [Improvement] Better comments for some map variables

--- pkg/parser/maps.go
+++ pkg/parser/maps.go
@@ -1,18 +1,27 @@
 package parser
 
 var (
-	// Active services and their variants
-	farmsMap = map[string][]string{}
+	// Map [(Node|Port) Service] to []{ farm names }
+	farmsPerServiceMap = map[string][]string{}
 
-	// Active endpoints per service
-	portsMap = map[string][]string{}
+	// Map [ExternalIP (Service|Endpoints)] to []{ farm names }
+	farmsPerExternalIPResourceMap = map[string][]string{}
 
-	// Active nodeports per service
-	nodePortMap = map[string][]string{}
+	// Map [Endpoints] to []{ farm names }
+	farmsPerEndpointMap = map[string][]string{}
 
-	// Active externalIPs per service
-	externalIPsMap = map[string][]string{}
+	// Map [Endpoints (farms)] to []{ backend names }
+	backendsPerFarm = map[string][]string{}
 
-	// Maxconns per backend in a farm
-	maxConnsMap = map[string]map[string]string{}
+	// Map [backend name] to "maxconn" value
+	maxConnsMap = map[string]string{}
 )
+
+func existsFarm(serviceName string, expectedFarmName string) bool {
+	for _, farmName := range farmsPerServiceMap[serviceName] {
+		if farmName == expectedFarmName {
+			return true
+		}
+	}
+	return false
+}
