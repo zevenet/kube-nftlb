65403b33c751b401fcb2f7169b2c312c1f44786f
AquoDev <14019792+AquoDev@users.noreply.github.com>
2020-10-06 14:04:19 +0200

    [Improvement] Use new Nftlb struct for JSON

--- pkg/parser/json.go
+++ pkg/parser/json.go
@@ -3,14 +3,16 @@ package parser
 import (
 	"encoding/json"
 	"strings"
+
+	"github.com/zevenet/kube-nftlb/pkg/types"
 )
 
-// StructAsJSON parses a given struct and returns a JSON string that can be interpreted by nftlb.
-func StructAsJSON(data interface{}) (string, error) {
+// NftlbAsJSON parses a given Nftlb struct and returns a JSON string that can be interpreted by nftlb.
+func NftlbAsJSON(data *types.Nftlb) (string, error) {
 	indentedJSON, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return "", err
 	}
 	// Add spaces before every :, it's required by nftlb
-	return strings.Replace(string(indentedJSON), "\":", "\" :", -1), nil
+	return strings.ReplaceAll(string(indentedJSON), "\":", "\" :"), nil
 }
