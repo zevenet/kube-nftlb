87b2b747831d683c9541bb87563de27b51bc8844
AquoDev <14019792+AquoDev@users.noreply.github.com>
2020-10-26 10:14:07 +0100

    [Bug fix] Fixed error dereferencing a pointer

--- pkg/parser/endpoints.go
+++ pkg/parser/endpoints.go
@@ -11,56 +11,65 @@ import (
 
 // EndpointsAsPaths sends backend paths through a channel to the controller. The controller then deletes every backend.
 func EndpointsAsPaths(endpoints *corev1.Endpoints, pathsChan chan<- string) {
-	for _, backendName := range backendsPerEndpoints[endpoints.Name] {
-		pathsChan <- fmt.Sprintf("farms/%s/backends/%s", endpoints.Name, backendName)
+	for _, farmName := range farmsPerService[endpoints.Name] {
+		for _, backendName := range backendsPerFarm[farmName] {
+			pathsChan <- fmt.Sprintf("farms/%s/backends/%s", farmName, backendName)
+		}
+		delete(backendsPerFarm, farmName)
 	}
 
-	delete(backendsPerEndpoints, endpoints.Name)
 	close(pathsChan)
 }
 
 // EndpointsAsNftlb reads a Endpoints object and returns a filled Nftlb struct.
 func EndpointsAsNftlb(endpoints *corev1.Endpoints) *types.Nftlb {
-	farm := types.Farm{
-		Name:     endpoints.Name,
-		Backends: make([]types.Backend, 0),
+	nftlb := &types.Nftlb{
+		Farms: make([]types.Farm, 0),
 	}
-	backendsPerEndpoints[endpoints.Name] = make([]string, 0)
-
-	var wg sync.WaitGroup
 
 	for idxSubset, subset := range endpoints.Subsets {
-		for idxPort := range subset.Ports {
-			for idxAddress := range subset.Addresses {
-				// Add a lock for every EndpointAddress
-				wg.Add(1)
+		// 1 EndpointPort (k8s) = 1 Farm (nftlb)
+		for idxPort, port := range subset.Ports {
+			farm := types.Farm{
+				Name:     FormatName(endpoints.Name, port.Name),
+				Backends: make([]types.Backend, len(subset.Addresses)),
+			}
+			backendsPerFarm[farm.Name] = make([]string, len(subset.Addresses))
 
-				// 1 EndpointAddress for every EndpointPort in a EndpointSubset (k8s) = 1 Backend (nftlb)
-				go func(epSubset *corev1.EndpointSubset, epPort *corev1.EndpointPort, epAddress *corev1.EndpointAddress, idxSubset int, idxPort int, idxAddress int) {
+			// Add a lock for every EndpointAddress
+			wg := new(sync.WaitGroup)
+			wg.Add(len(subset.Addresses))
+
+			// 1 EndpointAddress for every EndpointPort in a EndpointSubset (k8s) = 1 Backend (nftlb)
+			for idxAddress := range subset.Addresses {
+				//fmt.Printf("len(addresses): %d, idxAddress: %d | %s\n", len(subset.Addresses), idxAddress, farm.Name)
+				go func(epPort *corev1.EndpointPort, epAddress *corev1.EndpointAddress, idxAddress int) {
 					defer wg.Done()
 
 					backend := types.Backend{
-						Name:   fmt.Sprintf("%s--subset-%d--port-%d--address-%d", endpoints.Name, idxSubset, idxPort, idxAddress),
 						IPAddr: epAddress.IP,
 						State:  "up",
 						Port:   fmt.Sprint(epPort.Port),
 					}
 
-					farm.Backends = append(farm.Backends, backend)
+					if epAddress.TargetRef != nil {
+						backend.Name = FormatName(epAddress.TargetRef.Name, epPort.Name)
+					} else {
+						backend.Name = FormatName(endpoints.Name, epPort.Name)
+					}
 
-					backendsPerEndpoints[endpoints.Name] = append(backendsPerEndpoints[endpoints.Name], backend.Name)
-				}(&endpoints.Subsets[idxSubset], &subset.Ports[idxPort], &subset.Addresses[idxAddress], idxSubset, idxPort, idxAddress)
+					farm.Backends[idxAddress] = backend
+					backendsPerFarm[farm.Name][idxAddress] = backend.Name
+				}(&endpoints.Subsets[idxSubset].Ports[idxPort], &endpoints.Subsets[idxSubset].Addresses[idxAddress], idxAddress)
 			}
+
+			// Wait until all EndpointPort locks are released
+			wg.Wait()
+
+			nftlb.Farms = append(nftlb.Farms, farm)
 		}
 	}
 
-	// Wait until all locks are released
-	wg.Wait()
-
 	// Return a filled Nftlb struct
-	return &types.Nftlb{
-		Farms: []types.Farm{
-			farm,
-		},
-	}
+	return nftlb
 }
