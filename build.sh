4fd096f93b5c15f6846887dbb93da7849889673e
AquoDev <14019792+AquoDev@users.noreply.github.com>
2020-10-27 09:05:31 +0100

    [Improvement] Use local Docker image when built locally

--- build.sh
+++ build.sh
@@ -18,11 +18,8 @@ fi
 #eval $(minikube docker-env)
 
 # The container image will be built using its Dockerfile. Minikube will use this image later to make the container.
-docker image build --no-cache -t kube-nftlb -f $DOCKER_PATH/Dockerfile --build-arg DOCKER_PATH="$DOCKER_PATH" .
+docker image build --no-cache -t zevenet/kube-nftlb:latest -f $DOCKER_PATH/Dockerfile --build-arg DOCKER_PATH="$DOCKER_PATH" .
 
 # Clean residual files and intermediate containers.
 rm $DOCKER_PATH/nftlb.deb
 docker image prune -f --filter label=stage=intermediate
-
-# Every nftlb rule should be flushed before creating kube-nftlb. Do it at your own risk.
-#nft flush table nftlb
